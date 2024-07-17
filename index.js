2f1518266f796c47d0a0e832a2013c28eb6dfa7a
Andrei <andreiflausino@live.com>
2024-07-16 22:59:33 -0300

    Project done

---
+++ index.js
@@ -0,0 +1,55 @@
+let http = require("http");
+let url = require("url");
+let path = require("path");
+let fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      if (err) {
+        fs.readFile(path.join(__dirname, "404.html"), (err, data) => {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(data);
+        });
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+
+  if (req.url === "/about") {
+    fs.readFile(path.join(__dirname, "about.html"), (err, data) => {
+      if (err) {
+        fs.readFile(path.join(__dirname, "404.html"), (err, data) => {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(data);
+        });
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+
+  if (req.url === "/contact-me") {
+    fs.readFile(path.join(__dirname, "contact-me.html"), (err, data) => {
+      if (err) {
+        fs.readFile(path.join(__dirname, "404.html"), (err, data) => {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(data);
+        });
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  } else {
+    fs.readFile(path.join(__dirname, "404.html"), (err, data) => {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+});
+
+const PORT = 8080;
+
+server.listen(PORT);
